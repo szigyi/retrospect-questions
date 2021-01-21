2cbe231368b82c15d65d5e79f96abd24e688c9b9
BerniVarga <bvarga@uw.co.uk>
2021-01-21 22:45:33 +0000

    add in a couple of more questions

--- engine.js
+++ engine.js
@@ -65,11 +65,47 @@ function questions(happyOnlyMode) {
     }
     const happy = [
         'What made you happy today?',
-        'What did you do today that makes you proud?'
+        'What did you do today that makes you proud?',
+        'How did you care about yourself today?',
+        'What are the things that you did well today?',
+        'What made you smile today?',
+        'Who made you feel nice today?',
+        'Whom did you enjoy your time with today?',
+        'What are you grateful for today?',
+        'How was your body feeling today?',
+        'How did you care of your body today?',
+        'Did you daydream about something today? What was it?',
+        'What did you enjoy about your work today?',
+        'Did you learn something new today?',
+        'Did you notice anything new in your local area today?',
+        'What brought you peace today?',
+        'How did you motivate yourself today?',
+        'Was there anything you did better today, than a while ago?',
+        'What did you do to care about somebody else today?',
+        'Who helped you today?',
+        'Whom did you help today?',
+        'Did you receive a compliment today?',
+        'Did you give any compliments today?',
+        'What did your partner do today, that made you feel loved?',
+        'What did you do today, to make your partner feel loved?'
     ]
     const sad = [
         'What made you sad today?',
-        'What made you angry today?'
+        'What made you angry today?',
+        'How were you feeling today?',
+        'Where did your thoughts wonder today?',
+        'Were you afraid of anything today?',
+        'Would you change anything you did today?',
+        'Was there anything you wished for today?',
+        'Did anything cause you disappointment today?',
+        'Was there anything that really bothered you today?',
+        'If there would be one thing you could change in your house, what would that be?',
+        'What occuppied most of your free time today?',
+        'Did anything happen today, that was important to you?',
+        'How was your day?',
+        'Is there anything you wanted to do today, and you decided not to?',
+        'Did anybody made you feel uneasy today?',
+        'Is there anything you want to be better at?'
     ]
 
     if (happyOnlyMode) {

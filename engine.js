f5283ff97e9d5131c4450a8ad77d0ffe52a05e6f
Sabi <szigyi.szogyi@gmail.com>
2022-12-23 14:35:39 +0000

    use tags for questions instead of dedicated categories

--- engine.js
+++ engine.js
@@ -38,7 +38,7 @@ class DeckApp {
     #createCard(id, question) {
         return `
         <div id="${id}" class="card">
-            <div class="card-text">${question}</div>
+            <div class="card-text">${question.question}</div>
         </div>
         `
     }
@@ -53,6 +53,20 @@ class DeckApp {
     }
 }
 
+class Q {
+    constructor(question, tags) {
+        this.question = question
+        this.tags = tags
+    }
+}
+
+const Tag = {
+    Happy: Symbol("happy"),
+    Sad: Symbol("sad"),
+    Partner: Symbol("partner"),
+    Childhood: Symbol("childhood")
+}
+
 function questions(happyOnlyMode) {
     function shuffle(a) {
         for (let i = a.length - 1; i > 0; i--) {
@@ -61,54 +75,55 @@ function questions(happyOnlyMode) {
         }
         return a;
     }
-    const happy = [
-        'What made you happy today?',
-        'What did you do today that made you proud?',
-        'How did you look after yourself today?',
-        'What did you do well today?',
-        'What made you smile today?',
-        'Who made you feel nice today?',
-        'Whom did you enjoy your time with today?',
-        'What are you grateful for today?',
-        'How was your body feeling today?',
-        'How did you look after your body today?',
-        'Did you daydream about something today? What was it?',
-        'What did you enjoy about your work today?',
-        'Did you learn something new today?',
-        'Did you notice anything new in your local area today?',
-        'What brought you peace today?',
-        'How did you motivate yourself today?',
-        'Was there anything you did better today, than a while ago?',
-        'What did you do to care about somebody else today?',
-        'Who helped you today?',
-        'Whom did you help today?',
-        'Did you receive a compliment today?',
-        'Did you give any compliments today?',
-        'What did your partner do today, that made you feel loved?',
-        'What did you do today, to make your partner feel loved?'
-    ]
-    const sad = [
-        'What made you sad today?',
-        'What made you angry today?',
-        'How were you feeling today?',
-        'Where did your thoughts wonder today?',
-        'Were you afraid of anything today?',
-        'Would you change anything you did today?',
-        'Was there anything you wished for today?',
-        'Did anything cause you disappointment today?',
-        'Was there anything that really bothered you today?',
-        'If there would be one thing you could change in your house, what would that be?',
-        'What occupied most of your free time today?',
-        'Did anything happen today, that was important to you?',
-        'How was your day?',
-        'Is there anything you wanted to do today, and you decided not to?',
-        'Did anybody made you feel uneasy today?',
-        'Is there anything you want to be better at?'
+    const qs = [
+        new Q('What made you happy today?', [Tag.Happy]),
+        new Q('What did you do today that made you proud?', [Tag.Happy]),
+        new Q('How did you look after yourself today?', [Tag.Happy]),
+        new Q('What did you do well today?', [Tag.Happy]),
+        new Q('What made you smile today?', [Tag.Happy]),
+        new Q('Who made you feel nice today?', [Tag.Happy]),
+        new Q('Whom did you enjoy your time with today?', [Tag.Happy]),
+        new Q('What are you grateful for today?', [Tag.Happy]),
+        new Q('How was your body feeling today?', [Tag.Happy]),
+        new Q('How did you look after your body today?', [Tag.Happy]),
+        new Q('Did you daydream about something today? What was it?', [Tag.Happy]),
+        new Q('What did you enjoy about your work today?', [Tag.Happy]),
+        new Q('Did you learn something new today?', [Tag.Happy]),
+        new Q('Did you notice anything new in your local area today?', [Tag.Happy]),
+        new Q('What brought you peace today?', [Tag.Happy]),
+        new Q('How did you motivate yourself today?', [Tag.Happy]),
+        new Q('Was there anything you did better today, than a while ago?', [Tag.Happy]),
+        new Q('What did you do to care about somebody else today?', [Tag.Happy]),
+        new Q('Who helped you today?', [Tag.Happy]),
+        new Q('Whom did you help today?', [Tag.Happy]),
+        new Q('Did you receive a compliment today?', [Tag.Happy]),
+        new Q('Did you give any compliments today?', [Tag.Happy]),
+        new Q('What did your partner do today, that made you feel loved?', [Tag.Happy]),
+        new Q('What did you do today, to make your partner feel loved?', [Tag.Happy]),
+        new Q('What made you sad today?', [Tag.Sad]),
+        new Q('What made you angry today?', [Tag.Sad]),
+        new Q('How were you feeling today?', [Tag.Sad]),
+        new Q('Where did your thoughts wonder today?', [Tag.Sad]),
+        new Q('Were you afraid of anything today?', [Tag.Sad]),
+        new Q('Would you change anything you did today?', [Tag.Sad]),
+        new Q('Was there anything you wished for today?', [Tag.Sad]),
+        new Q('Did anything cause you disappointment today?', [Tag.Sad]),
+        new Q('Was there anything that really bothered you today?', [Tag.Sad]),
+        new Q('If there would be one thing you could change in your house, what would that be?', [Tag.Sad]),
+        new Q('What occupied most of your free time today?', [Tag.Sad]),
+        new Q('Did anything happen today, that was important to you?', [Tag.Sad]),
+        new Q('How was your day?', [Tag.Sad]),
+        new Q('Is there anything you wanted to do today, and you decided not to?', [Tag.Sad]),
+        new Q('Did anybody made you feel uneasy today?', [Tag.Sad]),
+        new Q('Is there anything you want to be better at?', [Tag.Sad])
     ]
 
     if (happyOnlyMode) {
-        return shuffle(happy)
+        return qs.filter(function(el) {
+            return happyOnlyMode && el.tags.includes(Tag.Happy)
+        })
     } else {
-        return shuffle(happy.concat(sad))
+        return qs
     }
+
 }
